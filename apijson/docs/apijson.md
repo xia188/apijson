3c31a87a515679f8ecba974cb7b3d757adc543f8
夏哥188 <18810761776@qq.com>
2024-06-11 14:43:39 +0800

    数据库依赖可选，由外部指定

--- apijson/docs/apijson.md
+++ apijson/docs/apijson.md
@@ -1,3 +1,8 @@
 ### 移除servlet依赖
 
 有些web框架如solon.web、hutool.http、light-4j等并不是servlet架构，apijson提供HttpSession供外部适配。
+
+### 依赖fastjson，其他可选
+
+UnitAutoApp依赖classgraph，需要时再引入依赖
+APIJSONSQLExecutor依赖postgresql，调整代码以便缺少相关类时也可以运行
--- src/main/java/apijson/framework/APIJSONSQLExecutor.java
+++ src/main/java/apijson/framework/APIJSONSQLExecutor.java
@@ -37,35 +37,37 @@ import apijson.orm.SQLConfig;
  */
 public class APIJSONSQLExecutor<T extends Object> extends AbstractSQLExecutor<T> {
 	public static final String TAG = "APIJSONSQLExecutor";
+	public static boolean hasPostgresql = false;
 
 	static {
 		try { //加载驱动程序
-			Log.d(TAG, "尝试加载 MySQL 8 驱动 <<<<<<<<<<<<<<<<<<<<< ");
+//			Log.d(TAG, "尝试加载 MySQL 8 驱动 <<<<<<<<<<<<<<<<<<<<< ");
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Log.d(TAG, "成功加载 MySQL 8 驱动！>>>>>>>>>>>>>>>>>>>>>");
 		}
 		catch (ClassNotFoundException e) {
 			Log.e(TAG, "加载 MySQL 8 驱动失败，请检查 pom.xml 中 mysql-connector-java 版本是否存在以及可用 ！！！");
-			e.printStackTrace();
+//			e.printStackTrace();
 
 			try { //加载驱动程序
-				Log.d(TAG, "尝试加载 MySQL 7 及以下版本的 驱动 <<<<<<<<<<<<<<<<<<<<< ");
+//				Log.d(TAG, "尝试加载 MySQL 7 及以下版本的 驱动 <<<<<<<<<<<<<<<<<<<<< ");
 				Class.forName("com.mysql.jdbc.Driver");
 				Log.d(TAG, "成功加载 MySQL 7 及以下版本的 驱动！>>>>>>>>>>>>>>>>>>>>> ");
 			}
 			catch (ClassNotFoundException e2) {
 				Log.e(TAG, "加载 MySQL 7 及以下版本的 驱动失败，请检查 pom.xml 中 mysql-connector-java 版本是否存在以及可用 ！！！");
-				e2.printStackTrace();
+//				e2.printStackTrace();
 			}
 		}
 
 		try { //加载驱动程序
-			Log.d(TAG, "尝试加载 PostgresSQL 驱动 <<<<<<<<<<<<<<<<<<<<< ");
+//			Log.d(TAG, "尝试加载 PostgresSQL 驱动 <<<<<<<<<<<<<<<<<<<<< ");
 			Class.forName("org.postgresql.Driver");
 			Log.d(TAG, "成功加载 PostgresSQL 驱动！>>>>>>>>>>>>>>>>>>>>> ");
+			hasPostgresql = true;
 		}
 		catch (ClassNotFoundException e) {
-			e.printStackTrace();
+//			e.printStackTrace();
 			Log.e(TAG, "加载 PostgresSQL 驱动失败，请检查 libs 目录中 postgresql.jar 版本是否存在以及可用 ！！！");
 		}
 		
@@ -74,7 +76,7 @@ public class APIJSONSQLExecutor<T extends Object> extends AbstractSQLExecutor<T>
 
 	@Override
 	public PreparedStatement setArgument(@NotNull SQLConfig<T> config, @NotNull PreparedStatement statement, int index, Object value) throws SQLException {
-		if (config.isPostgreSQL() && JSON.isBooleanOrNumberOrString(value) == false) {
+		if (hasPostgresql && config.isPostgreSQL() && JSON.isBooleanOrNumberOrString(value) == false) {
 			PGobject o = new PGobject();
 			o.setType("jsonb");
 			o.setValue(value == null ? null : value.toString());
@@ -92,7 +94,7 @@ public class APIJSONSQLExecutor<T extends Object> extends AbstractSQLExecutor<T>
 		
 		Object value = super.getValue(config, rs, rsmd, tablePosition, table, columnIndex, lable, childMap);
 
-		return value instanceof PGobject ? JSON.parse(((PGobject) value).getValue()) : value;
+		return (hasPostgresql && value instanceof PGobject) ? JSON.parse(((PGobject) value).getValue()) : value;
 	}
 
 	// 支持 !key 反选字段 和 字段名映射，依赖插件 https://github.com/APIJSON/apijson-column
